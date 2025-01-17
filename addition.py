c72445d55ea76c245283c00dc9a955cef0ec0385
Dnyaneshwar <dshinde757@gmail.com>
2025-01-18 02:34:52 +0530

    Enhance add function to support custom separators defined by "//" and newlines

--- addition.py
+++ addition.py
@@ -1,9 +1,15 @@
 def add(numbers: str) -> int:
     """
-    Add numbers in a string separated by commas
+    Add numbers in a string separated by commas or newlines
+    function also supports custom separators
+    custom separators are defined by a string starting with "//" and ending with "\n"
     """
     if numbers == "":
         return 0
+    elif "//" in numbers:
+        separator, numbers = numbers.split("\n")
+        separator = separator[2:]
+        return sum([int(number) for number in numbers.split(separator)])
     elif "," in numbers or "\n" in numbers:
         numbers = numbers.replace("\n", ",")
         return sum([int(number) for number in numbers.split(",")])
